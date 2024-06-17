a5dba7ad7a71be017608ddebb992eff811fe3861
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-06-17 14:58:31 +0200

    test commit

---
+++ u5d1/src/main/java/lorenzofoschetti/u5d1/U5d1Application.java
@@ -0,0 +1,15 @@
+package lorenzofoschetti.u5d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(U5d1Application.class, args);
+        System.out.println("ciao");
+    }
+
+}
